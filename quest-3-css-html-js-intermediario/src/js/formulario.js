5e1dff4c3de254d7991e1284d7b6f094ca2cc7c8
Samuel Coelho <samuel4050@hotmail.com>
2024-07-23 09:40:11 -0300

    Modificações

--- quest-3-css-html-js-intermediario/src/js/formulario.js
+++ quest-3-css-html-js-intermediario/src/js/formulario.js
@@ -9,12 +9,15 @@ btnEnviar[0].addEventListener('click', (e) =>{
 })
 
 function validateInput(element) {
+  const errorMessage = element.nextElementSibling;
     if (element.value.trim() !== '') {
       element.classList.add('input-validado');
       element.classList.remove('input-invalido', 'input-default');
+      errorMessage.textContent = '';
     } else {
       element.classList.add('input-invalido');
       element.classList.remove('input-validado', 'input-default');
+      errorMessage.textContent = 'campo obrigatório';
     }
   }
 
@@ -22,7 +25,7 @@ function validateInput(element) {
     let isValid = true;
     inputs.forEach((element) => {
       validateInput(element);
-      if (element.classList.contains('input-error')) {
+      if (element.classList.contains('input-invalido')) {
         isValid = false;
       }
     });
